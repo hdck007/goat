b8ebd32b00015032c9740fc00faf26d45862fea6
Haridarshan Choudhary <haridarshanofficial@gmail.com>
2023-10-22 14:18:46 +0530

    chore: removed unused files

--- index.js
+++
@@ -1,7 +0,0 @@
-// console.log("This is Haridarshan");
-
-// const button = document.querySelector("#myid");
-
-// button.addEventListener("click", () => {
-//   document.body.style.background = "red";
-// });
